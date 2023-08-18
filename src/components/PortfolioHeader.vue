10a961fb9adc676d93184e7edab89748430516c6
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2023-08-19 02:39:57 +1200

    Implemented project page title

--- src/components/PortfolioHeader.vue
+++ src/components/PortfolioHeader.vue
@@ -8,35 +8,11 @@ defineProps({ msg: {
 
 <template>
     <div class="bg-black fixed w-full top-0">
-      <div class="flex">
-        <h1 class="text-2x1 bg-gradient-to-r from-purple-400 via-gray-600 to-purple-400 text-transparent
+      <div class="flex items-center h-16 px-4">
+        <h1 class="text-4xl font-semibold bg-gradient-to-r from-purple-400 via-gray-600 to-purple-400 text-transparent
         bg-clip-text animate-header-gradient bg-1000">{{ msg }}</h1>
-          <router-link :to="'/projects/test'" class="text-purple-500">Go to Projects</router-link>
+          <router-link :to="'/projects/test'" class="text-purple-500 ml-auto">Go to Projects</router-link>
       </div>
     </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,10 +1,19 @@
 <template>
-    <div class="grid grid-cols-4 gap-5">
-        <a v-for="project in projects" :href="project['Link']">
+    <div class="relative flex items-center h-2/3">
+        <img class="filter brightness-[.3] absolute top-0 h-full w-full object-cover" :src="curImage">
+            <transition name="fade" mode="out-in">
+                <div :key="curImage">
+                    <img class="filter brightness-[.3] absolute top-0 h-full w-full object-cover" :src="curImage">
+                </div>
+            </transition>
+        <div class="absolute px-40 text-2xl text-gray-100">Steam Projects</div>
+    </div>
+    <div class="grid grid-cols-4 gap-5 px-8">
+        <a v-for="project in projects" :href="project.Link">
             <div class="bg-black max-w-md rounded overflow-hidden shadow-lg shadow-gray-600">
-                <img :src="project['Data']['Image']" v-if="project['Data']['Image']">
+                <img :src="project.Data.Image" v-if="project.Data.Image">
                 <div class="flex items-center h-16 mx-4">
-                    <span class="text-lg font-extrabold text-gray-400">{{ project['Name'] }}</span>
+                    <span class="text-lg font-extrabold text-gray-400">{{ project.Name }}</span>
                 </div>
             </div>
         </a>
@@ -20,27 +29,49 @@ export default {
     name: "Projects",
     data() {
         return {
-            projects: Empty
+            projects: [],
+            imageIndex: 0
         };
     },
     computed: {
         projectType() {
             return this.$route.params.projectType;
+        },
+        imageData() {
+            return this.projects.filter(p => p.Data.Image).map(p => p.Data.Image);
+        },
+        curImage(){
+            return this.imageData[this.imageIndex];
+        }
+    },
+    methods: {
+        nextSlide() {
+            this.imageIndex = (this.imageIndex + 1) % this.imageData.length;
+        },
+        startSlides() {
+            setInterval(this.nextSlide, 4000);
         }
     },
     async mounted() {
+        Empty.fetchData().then(this.projects);
         switch (this.projectType) {
             case 'steam':
                 this.projects = await SteamProjects.fetchData();
                 break;
             case 'itchio':
-                this.projects = ItchioProjects;
+                this.projects = await ItchioProjects.fetchData();
                 break;
         }
+        this.startSlides();
     }
 };
 </script>
 
 <style scoped>
-
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 2s;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
 </style>
