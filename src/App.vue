22b52de4f0f1fa4d1c9e591d55ad0d11282b738a
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2023-08-19 02:39:50 +1200

    Converted project to TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+interface Config {
+  plugins: any[];
+  resolve: {
+    alias: Record<string, string>;
+  };
+}
+
+const config: Config = {
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag === "main-component"
+        }
+      },
+      script:{
+        refSugar: true
+      }
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+}
+
+export default config;
