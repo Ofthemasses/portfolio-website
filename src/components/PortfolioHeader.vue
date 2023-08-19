27e2eb3e62a0224e12b24f308513f51a8f7acff3
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-20 01:35:27 +1200

    Changed header styling

--- src/components/PortfolioHeader.vue
+++ src/components/PortfolioHeader.vue
@@ -10,8 +10,7 @@ defineProps({ msg: {
     <div class="bg-black fixed w-full z-50">
       <div class="flex items-center h-16 px-4">
         <a href="/#">
-          <h1 class="text-4xl font-semibold hover:bg-gradient-to-r hover:from-purple-400 hover:via-gray-300 hover:to-purple-400 text-transparent
-          bg-clip-text bg-gray-300 animate-header-gradient bg-1000">{{ msg }}</h1>
+          <h1 class="text-4xl text-gray-200">{{ msg }}</h1>
         </a>
           <router-link :to="'/projects/test'" class="text-purple-500 ml-auto">Go to Projects</router-link>
       </div>
