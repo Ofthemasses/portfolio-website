2db3bba17e12eb3ea098fa0bd76270a9bc16fd89
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-19 19:16:21 +1200

    Implemented itchio api

--- src/api/extensions/itchio.ts
+++ src/api/extensions/itchio.ts
@@ -1,12 +1,29 @@
 import { ProjectApi } from "../project-api";
+import axios from "axios";
 
 export const ItchioProjects = new class extends ProjectApi {
-    fetchData(): Promise<[{Name: String, Link: String, Data: String}]> {
-        return new Promise(r => r([{"Name": "No Projects", "Link": "", "Data": ""}]));
+    async fetchData(): Promise<[{Name: String, Link: String, Data: String}]> {
+        const rawdata = await this.getItchGames(); // Wait for data to be retrieved
+        const data: {Name: String, Link: String, Data: {}}[] = [];
+        for (let game of rawdata["games"]){
+            data.push({
+                Name: game["title"],
+                Link: game["url"],
+                Data: {Image: game["cover_url"]}
+            });
+        }
+        return data;
     }
 
+    async getItchGames(): Promise<any> {
+        let url: string = `https://toru.studio/toru-api-handler/itchio/getjson.php`;
+        const response = await axios.get(url, {
+            responseType: "json"
+        });
+        return response.data;
+    }
     public apiName(): String {
-        return "";
+        return "ITCH.IO GAMES";
     }
 
 }
