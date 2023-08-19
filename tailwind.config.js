7116e1187d2ceaa4993677d54b6b4e4a85ea1c9e
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-20 02:09:05 +1200

    Removed unused tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,18 +10,6 @@ module.exports = {
             // Override base font
             'sans': 'Martian Mono Variable, sans-serif'
         },
-        keyframes: {
-            'animated-gradient': {
-                '0%': { backgroundPosition: '0% 50%' },
-                '100%': { backgroundPosition: '200% 50%' },
-            }
-        },
-        backgroundSize: {
-            '1000': '200%'
-        },
-        animation: {
-            'header-gradient': 'animated-gradient 5s linear infinite'
-        }
     },
   },
   plugins: [],
