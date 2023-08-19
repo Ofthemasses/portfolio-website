aec228244ae12d3c62eabe147ec9e7ed6932c36b
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-20 02:19:35 +1200

    Created home component and added it to routing

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
 import Projects from "./components/Projects.vue";
+import Home from "./components/Home.vue";
 import './style.css';
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
-        { path: '/projects/:projectType', name: 'Projects', component: Projects }
+        { path: '/projects/:projectType', name: 'Projects', component: Projects },
+        { path: '/', name: 'Home', component: Home}
     ]
 });
 
