670591145f3646fe7810318a6c5b0f1867ee0921
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-20 01:31:49 +1200

    Removed unused modules

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script lang="ts" setup>
 import PortfolioHeader from './components/PortfolioHeader.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-      <PortfolioHeader msg="My Portfolio" />
-
+      <PortfolioHeader msg="Portfolio" />
   </header>
 
   <main class="py-16 h-screen">
