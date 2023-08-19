5ecb07ca218746f869f70305c685c15f221a119d
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-20 02:18:11 +1200

    Adjusted colour and margins on navigation

--- src/components/PortfolioHeader.vue
+++ src/components/PortfolioHeader.vue
@@ -10,11 +10,11 @@ defineProps({ msg: {
     <div class="bg-black fixed w-full z-50">
       <div class="flex items-center h-16 px-4">
         <a href="/#">
-          <h1 class="text-4xl text-gray-200">{{ msg }}</h1>
+          <h1 class="text-4xl text-gray-100">{{ msg }}</h1>
         </a>
-        <div class="text-white ml-auto px-6">
-          <router-link :to="'/projects/github'" class="pl-6">GitHub</router-link>
-          <router-link :to="'/projects/steam'" class="pl-6">Steam</router-link>
+        <div class="text-gray-100 ml-auto px-6">
+          <router-link :to="'/projects/github'" class="ml-6">GitHub</router-link>
+          <router-link :to="'/projects/steam'" class="ml-6">Steam</router-link>
           <router-link :to="'/projects/itchio'" class="pl-6">Itch.io</router-link>
       </div>
     </div>
