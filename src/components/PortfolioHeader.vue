10e6dff94596931f3318619d288f31025e6d6e52
Finlay Metcalfe <43369124+ofthemasses@users.noreply.github.com>
2023-08-19 23:10:14 +1200

    Added back link to home

--- src/components/PortfolioHeader.vue
+++ src/components/PortfolioHeader.vue
@@ -9,8 +9,10 @@ defineProps({ msg: {
 <template>
     <div class="bg-black fixed w-full z-50">
       <div class="flex items-center h-16 px-4">
-        <h1 class="text-4xl font-semibold bg-gradient-to-r from-purple-400 via-gray-600 to-purple-400 text-transparent
-        bg-clip-text animate-header-gradient bg-1000">{{ msg }}</h1>
+        <a href="/#">
+          <h1 class="text-4xl font-semibold hover:bg-gradient-to-r hover:from-purple-400 hover:via-gray-300 hover:to-purple-400 text-transparent
+          bg-clip-text bg-gray-300 animate-header-gradient bg-1000">{{ msg }}</h1>
+        </a>
           <router-link :to="'/projects/test'" class="text-purple-500 ml-auto">Go to Projects</router-link>
       </div>
     </div>
