7a379cb761482e9799ade7e6499ad8724cab6ff6
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2024-03-03 11:07:29 +1300

    Implemented a redirect to auto generated code docs

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
 import Projects from "./components/Projects.vue";
 import Home from "./components/Home.vue";
+import Docs from "./components/Docs.vue";
 import './style.css';
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         { path: '/projects/:projectType', name: 'Projects', component: Projects },
+        { path: '/vim-ventures/docs/:pathMatch(.*)*', component:Projects, beforeEnter: (to,from) => {
+            var path = to.path.split('/vim-ventures/docs').pop();
+            if (path.length == 1){ 
+                window.location.href = '/vim-ventures/docs/index.html';
+            } else {
+                window.location.href = '/vim-ventures/docs' + path;
+            }
+            return true;
+        }},
         { path: '/', name: 'Home', component: Home}
     ]
 });
