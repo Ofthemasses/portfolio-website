4877d297c6ca3b3bca6e39d869af428b38bffc7c
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2024-03-10 22:21:11 +1300

    Implemented styling for markdown pages

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <vue-markdown :source="markdownContent" :options="options"/>
+  <div class="relative flex justify-center items-center">
+    <h1 class="max-md:text-3xl text-5xl mt-8 mb-2 text-gray-100">{{this.$route.params.markdownPath }}</h1>
+  </div>
+  <div class="bg-gray-200 m-8 p-8 max-w rounded shadow-lg">
+    <vue-markdown class="markdown" :source="markdownContent" :options="options"/>
   </div>
 </template>
 
@@ -48,7 +51,64 @@ export default {
 };
 </script>
 
-<style scoped>
-/* Add any necessary styles for your component */
+<style>
+.markdown {
+    font-size: 16px;
+    line-height: 1.6;
+    color: #333;
+    padding: 20px;
+}
+
+.markdown h1 {
+    font-size: 24px;
+}
+
+.markdown h2 {
+    font-size: 22px;
+}
+
+.markdown h3 {
+    font-size: 20px;
+}
+
+.markdown p {
+    margin-bottom: 10px;
+}
+
+.markdown ul,
+.markdown ol {
+    margin-bottom: 10px;
+}
+
+.markdown li {
+    margin-bottom: 5px;
+}
+
+.markdown a {
+    color: #007bff;
+    text-decoration: none;
+}
+
+.markdown a:hover {
+    text-decoration: underline;
+}
+
+.markdown pre {
+    background-color: #f8f9fa;
+    padding: 10px;
+    border-radius: 5px;
+    overflow-x: auto;
+    margin-bottom: 15px;
+}
+
+.markdown code {
+    font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+}
+
+.markdown blockquote {
+    border-left: 4px solid #007bff;
+    padding-left: 15px;
+    margin-left: 0;
+}
 </style>
 
