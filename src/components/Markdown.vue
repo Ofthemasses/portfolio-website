c45105e208c8e0d7783e7f70509b136a6147673d
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2024-03-11 13:44:07 +1300

    Replaced vue-markdown-renderer with markdown-it for url parsing, implemented github-markdown-css theme

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -2,35 +2,21 @@
   <div class="relative flex justify-center items-center">
     <h1 class="max-md:text-3xl text-5xl mt-8 mb-2 text-gray-100">{{this.$route.params.markdownPath }}</h1>
   </div>
-  <div class="bg-gray-200 m-8 p-8 max-w rounded shadow-lg">
-    <vue-markdown class="markdown" :source="markdownContent" :options="options"/>
+  <div class="bg-gray-200 m-8 p-8 max-w rounded shadow-lg markdown-body" v-html="renderedMarkdown">
   </div>
 </template>
 
 <script>
-// Import the vue-markdown-render component
-import VueMarkdown from 'vue-markdown-render';
+import MarkdownIt from 'markdown-it';
 import axios from 'axios';
+import 'github-markdown-css/github-markdown-light.css';
 
 export default {
   name: 'Markdown',
-  components: {
-    VueMarkdown
-  },
   data() {
     return {
-      markdownContent: '[Hello, Markdown!](https://www.google.com/)',
-      options: {
-        renderer: {
-          rules: {
-          link_open: function (tokens, idx, options, env, self) {
-            const token = tokens[idx]
-            const href = token.attr[token.attrIndex('href')[1]]
-            return `<router-link to="markdown/${href}">${md.utils.escapeHtml(token.content)}</router-link>`;
-          } 
-          }
-        }
-      }
+      markdownContent: '',
+      renderedMarkdown: ''
     };
   },
   methods: {
@@ -43,72 +29,27 @@ export default {
       catch (e) {
         this.markdownContent = "Could not fetch markdown content: "+e;
       }
+      this.renderMarkdown();
     },
+    renderMarkdown() {
+      const md = MarkdownIt();
+      md.renderer.rules['link_open'] = function(tokens, idx, options, env, self){
+        const hrefIndex = tokens[idx].attrIndex('href');
+        let hrefVal = tokens[idx].attrs[hrefIndex][1];
+
+        const isExternal = new URL(hrefVal, window.location.href).hostname !== window.location.hostname;
+
+        if (!isExternal){
+          hrefVal = '/#/markdown/' + hrefVal;
+          tokens[idx].attrs[hrefIndex][1] = hrefVal;
+        }
+        return self.renderToken(tokens, idx, options);
+      }
+      this.renderedMarkdown = md.render(this.markdownContent);
+    }
   },
   mounted() {
     this.fetchMarkdownContent()
   }
 };
 </script>
-
-<style>
-.markdown {
-    font-size: 16px;
-    line-height: 1.6;
-    color: #333;
-    padding: 20px;
-}
-
-.markdown h1 {
-    font-size: 24px;
-}
-
-.markdown h2 {
-    font-size: 22px;
-}
-
-.markdown h3 {
-    font-size: 20px;
-}
-
-.markdown p {
-    margin-bottom: 10px;
-}
-
-.markdown ul,
-.markdown ol {
-    margin-bottom: 10px;
-}
-
-.markdown li {
-    margin-bottom: 5px;
-}
-
-.markdown a {
-    color: #007bff;
-    text-decoration: none;
-}
-
-.markdown a:hover {
-    text-decoration: underline;
-}
-
-.markdown pre {
-    background-color: #f8f9fa;
-    padding: 10px;
-    border-radius: 5px;
-    overflow-x: auto;
-    margin-bottom: 15px;
-}
-
-.markdown code {
-    font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
-}
-
-.markdown blockquote {
-    border-left: 4px solid #007bff;
-    padding-left: 15px;
-    margin-left: 0;
-}
-</style>
-
