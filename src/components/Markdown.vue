bb7d6233b5e0015b64c2eab1c35a0dc0655c5026
metcalfinl <finlaymetcalfe@hotmail.com>
2025-01-10 18:06:50 +1300

    Prepared markdown interpreter for multiple markdown pages

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative flex justify-center items-center">
-    <h1 class="max-md:text-3xl text-5xl mt-8 mb-2 text-gray-100">{{this.$route.params.markdownPath }}</h1>
+    <h1 class="max-md:text-3xl text-5xl mt-8 mb-2 text-gray-100">{{getDisplayValue()}}</h1>
   </div>
   <div class="bg-gray-200 m-8 p-8 max-w rounded shadow-lg markdown-body" v-html="renderedMarkdown">
   </div>
@@ -22,8 +22,7 @@ export default {
   methods: {
     async fetchMarkdownContent() {
       try {
-        console.log(this.$route.params.markdownPath);
-        const response = await axios.get('/vim-game-sdl2/'+this.$route.params.markdownPath+".md");
+        const response = await axios.get('/md/'+this.$route.params.markdownPath.join('/')+".md");
         this.markdownContent = response.data;
       }
       catch (e) {
@@ -33,6 +32,7 @@ export default {
     },
     renderMarkdown() {
       const md = MarkdownIt();
+	  const markdownPath = this.$route.params.markdownPath;
       md.renderer.rules['link_open'] = function(tokens, idx, options, env, self){
         const hrefIndex = tokens[idx].attrIndex('href');
         let hrefVal = tokens[idx].attrs[hrefIndex][1];
@@ -40,16 +40,25 @@ export default {
         const isExternal = new URL(hrefVal, window.location.href).hostname !== window.location.hostname;
 
         if (!isExternal){
-          hrefVal = '/#/markdown/' + hrefVal;
+          hrefVal = '/#/markdown/' + markdownPath.slice(0, -1).join('/') + '/' + hrefVal;
           tokens[idx].attrs[hrefIndex][1] = hrefVal;
         }
         return self.renderToken(tokens, idx, options);
       }
       this.renderedMarkdown = md.render(this.markdownContent);
-    }
+    },
+	getDisplayValue() {
+	  const markdownPath = this.$route.params.markdownPath;
+	  const length = markdownPath.length;
+
+	  return markdownPath.at(-1) === 'root'
+		? markdownPath.at(-2)
+		: markdownPath.at(-1);
+	}
   },
   mounted() {
     this.fetchMarkdownContent()
   }
+
 };
 </script>
